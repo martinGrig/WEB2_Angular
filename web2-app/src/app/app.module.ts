c58a0697025e3ba6b739991378c3d38b899774e3
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2019-09-11 15:18:53 +0200

    Component Employees

--- web2-app/src/app/app.module.ts
+++ web2-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
