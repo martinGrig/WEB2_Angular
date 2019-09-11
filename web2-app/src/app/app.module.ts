127f33f0796bc9df040e98dc2dea4ccd64fdc5e5
Martin M. Grigorov <marty7566@gmail.com>
2019-09-11 15:53:26 +0200

    department component

--- web2-app/src/app/app.module.ts
+++ web2-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DepartmentsComponent } from './departments/departments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule
