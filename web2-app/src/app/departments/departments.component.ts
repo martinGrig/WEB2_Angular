f40789b548448b93526aa74bf50429c31ac48edf
Martin M. Grigorov <marty7566@gmail.com>
2019-09-15 19:06:30 +0200

    departments

--- web2-app/src/app/departments/departments.component.ts
+++ web2-app/src/app/departments/departments.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoaderConfig } from '@angular/core';
+import {Department} from '../Models/Departments';
 
 @Component({
   selector: 'app-departments',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./departments.component.css']
 })
 export class DepartmentsComponent implements OnInit {
+  departments: Department[];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { 
+    this.departments = [
+      {
+        id: 1,
+        name : "HR",
+        location: "Tilburg"
 
+      },
+      {
+        id: 2,
+        name : "Security",
+        location: "Amsterdam"
+
+      },{
+        id: 3,
+        name : "Engineering",
+        location: "Eindhoven"
+
+      }
+    ]
+}
 }
