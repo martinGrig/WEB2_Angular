50ae86d35ec412f112b1443a753a279caa7bf996
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2019-09-15 14:56:02 +0200

    Merge conflict

--- web2-app/src/app/app.module.ts
+++ web2-app/src/app/app.module.ts
@@ -3,29 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-<<<<<<< HEAD
+ 
 import { EmployeesComponent } from './employees/employees.component';
-=======
-<<<<<<< HEAD
+
 import { DepartmentsComponent } from './departments/departments.component';
-=======
-import { TasksComponent } from './tasks/tasks.component';
->>>>>>> ba6d2a99e94ea31200ab4eafaf87dab7170942de
->>>>>>> ad035e8f482a227478f23fb3980bb70b98792b84
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-<<<<<<< HEAD
-    EmployeesComponent
-=======
-<<<<<<< HEAD
+
+    EmployeesComponent,
+
     DepartmentsComponent
-=======
-    TasksComponent
->>>>>>> ba6d2a99e94ea31200ab4eafaf87dab7170942de
->>>>>>> ad035e8f482a227478f23fb3980bb70b98792b84
+
   ],
   imports: [
     BrowserModule
