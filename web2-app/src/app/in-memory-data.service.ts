d00697f221079e701b08d0fb324adebf8d6de33d
Martin <Grigorov>
2019-09-29 19:18:53 +0200

    departments have an array of employees

--- web2-app/src/app/in-memory-data.service.ts
+++ web2-app/src/app/in-memory-data.service.ts
@@ -20,12 +20,12 @@ export class InMemoryDataService implements InMemoryDbService {
     ];
           
     const departments = [
-      { id: 1, name : "HR", location: "Tilburg", employees: 'John'},
-      { id: 2, name : "Security", location: "Amsterdam", employees: 'Lucy'},
-      { id: 3, name : "Engineering", location: "Eindhoven", employees: 'Veselin'},
-      { id: 4, name : "Accouting", location: "Tilburg", employees: 'Jax'},
-      { id: 5, name : "Sales", location: "Amsterdam", employees: 'Alexander' },
-      { id: 6, name : "Production", location: "Eindhoven", employees: 'Jerry'}
+      { id: 1, name : "HR", location: "Tilburg", employees: ['John']},
+      { id: 2, name : "Security", location: "Amsterdam", employees: ['Lucy']},
+      { id: 3, name : "Engineering", location: "Eindhoven", employees: ['Veselin']},
+      { id: 4, name : "Accouting", location: "Tilburg", employees: ['Jax']},
+      { id: 5, name : "Sales", location: "Amsterdam", employees: ['Alexander'] },
+      { id: 6, name : "Production", location: "Eindhoven", employees: ['Jerry']}
       ];
       
       const employees = [
--- web2-app/src/app/mock-departments.ts
+++ web2-app/src/app/mock-departments.ts
@@ -1,10 +1,10 @@
 import { Department } from './Departments';
 
-export const DEPARTMENTS: Department[] = [
-    { id: 1, name : "HR", location: "Tilburg", employees: 'John'},
-    { id: 2, name : "Security", location: "Amsterdam", employees: 'Lucy'},
-    { id: 3, name : "Engineering", location: "Eindhoven", employees: 'Veselin'},
-    { id: 4, name : "Accouting", location: "Tilburg", employees: 'Jax'},
-    { id: 5, name : "Sales", location: "Amsterdam", employees: 'Alexander' },
-    { id: 6, name : "Production", location: "Eindhoven", employees: 'Jerry'}
+export const DEPARTMENTS: Department[] =  [
+    { id: 1, name : "HR", location: "Tilburg", employees: ['John']},
+    { id: 2, name : "Security", location: "Amsterdam", employees: ['Lucy']},
+    { id: 3, name : "Engineering", location: "Eindhoven", employees: ['Veselin']},
+    { id: 4, name : "Accouting", location: "Tilburg", employees: ['Jax']},
+    { id: 5, name : "Sales", location: "Amsterdam", employees: ['Alexander'] },
+    { id: 6, name : "Production", location: "Eindhoven", employees: ['Jerry']}
     ];
