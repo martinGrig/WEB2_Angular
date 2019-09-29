4f9e653d63ef780bbf7877f1b67aefebb5df15cb
Ivanov,Dimitar D.A <dimitar.ivanov@student.fontys.nl>
2019-09-29 23:22:25 +0200

    Task properties fix

--- web2-app/src/app/mock-tasks.ts
+++ web2-app/src/app/mock-tasks.ts
@@ -1,14 +1,14 @@
 import { Task } from './task';
 
 export const TASKS: Task[] = [
-  { id: 11, name: 'Run'},
-  { id: 12, name: 'Jump'},
-  { id: 13, name: 'Climb'},
-  { id: 14, name: 'Fly'},
-  { id: 15, name: 'Swim'},
-  { id: 16, name: 'Drive'},
-  { id: 17, name: 'Cycle'},
-  { id: 18, name: 'Fight'},
-  { id: 19, name: 'Crouch'},
-  { id: 20, name: 'Sleep'}
+  { id: 1, name: 'Run', department: 'HR', employee: 'John', deadline: '13-11-2019'},
+  { id: 2, name: 'Jump', department: 'Security', employee: 'Lucy', deadline: '02-11-2019'},
+  { id: 3, name: 'Climb', department: 'Engineering', employee: 'Veselin', deadline: '07-11-2019'},
+  { id: 4, name: 'Fly', department: 'Accouting', employee: 'Jax', deadline: '14-11-2019'},
+  { id: 5, name: 'Swim', department: 'Sales', employee: 'Alexander', deadline: '11-11-2019'},
+  { id: 6, name: 'Drive', department: 'Production', employee: 'Jerry', deadline: '10-11-2019'},
+  { id: 7, name: 'Cycle', department: 'HR', employee: 'John', deadline: '22-11-2019'},
+  { id: 8, name: 'Fight', department: 'Security', employee: 'Lucy', deadline: '01-11-2019'},
+  { id: 9, name: 'Crouch', department: 'Engineering', employee: 'Veselin', deadline: '06-11-2019'},
+  { id: 10, name: 'Sleep', department: 'Accouting', employee: 'Jax', deadline: '26-11-2019'}
 ];
