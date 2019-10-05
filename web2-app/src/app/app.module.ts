215c68c8add6606d73ceca286806bf54c7d232d8
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2019-10-05 20:02:53 +0200

    Search for employees

--- web2-app/src/app/app.module.ts
+++ web2-app/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { TaskDetailComponent }  from './task-detail/task-detail.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
+
 import { DepartmentsComponent } from './departments/departments.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 import { DepartmentSearchComponent }  from './department-search/department-search.component';
@@ -25,6 +26,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 //Bootstrap Modules
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { EmployeeSearchComponent } from './employee-search/employee-search.component';
 
 
 @NgModule({
@@ -58,6 +60,8 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
     DepartmentSearchComponent,
 
     DashboardComponent,
+
+    EmployeeSearchComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
